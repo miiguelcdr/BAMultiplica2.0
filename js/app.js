5cb3abfdb45dacb8320d4b3a942322b66f8ebce4
JesusValera <miiguelcordero@gmail.com>
2023-11-17 00:02:02 -0300

    ultimo commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/**nav**/
+function abrirnav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function cerrarnav(){
+    document.getElementById("mobile-menu").style.width = "0";
+}
+
+
+    
+    
